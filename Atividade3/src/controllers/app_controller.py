6490924bbc9b1cd477ce38fc67e26ec5a410857a
Nat250 <64267713+Nat250@users.noreply.github.com>
2022-11-12 18:52:01 -0300

    Delete Atividade3 directory

--- Atividade3/src/controllers/app_controller.py
+++
@@ -1,8 +0,0 @@
-# Nome: Johannes Mattheus Krouwel   RA: 20.01248-9
-
-class App_Controller():
-    def loginStatus(confirmed):
-        if confirmed == True:
-            return True
-        else:
-            return False
